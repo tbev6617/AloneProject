5a856bd1141f558b2c91c83c0cbe92b6a0a97d2d
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-09-07 08:40:44 -0600

    Created my alone project and added my controller and runner

---
+++ src/alone/controller/AloneController.java
@@ -0,0 +1,9 @@
+package alone.controller;
+
+public class AloneController 
+{
+	public void start() 
+	{
+		System.out.println("I can write code all by myself");
+	}
+}
---
+++ src/alone/controller/AloneRunner.java
@@ -0,0 +1,10 @@
+package alone.controller;
+
+public class AloneRunner 
+{
+	public static void main(String[] args) 
+	{
+		AloneController myApp = new AloneController();
+		myApp.start();
+	}
+}
